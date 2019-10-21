4c28097745489fca76ca777af0107a735fd722f4
cesar.contreras <ccontreras@rmgperustaff.com>
2019-10-21 17:54:58 -0500

    [update] terminado clase 09

--- src/listOfCategories/index.js
+++ src/listOfCategories/index.js
@@ -2,10 +2,11 @@
 import React from 'react'
 import { Category } from '../category'
 import { Lista, Item } from './styles'
+import { categories } from '../../api/db.json'
 
 export const ListOfCategories = () =>
   <Lista>
     {
-      [1, 2, 3, 4, 5].map(category => <Item key={category}><Category /></Item>)
+      categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
     }
   </Lista>
