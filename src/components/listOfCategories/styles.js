a93500e88c340ffb929fef370333845104b1f7fa
cesar.contreras <ccontreras@rmgperustaff.com>
2019-10-28 17:55:26 -0500

    [update] notas y efectos

--- src/components/listOfCategories/styles.js
+++ src/components/listOfCategories/styles.js
@@ -18,7 +18,7 @@ export const Lista = Styled.ul`
     top: -20px;
     transform: scale(.5);
     z-index: 1;
-    ${fadeIn({ time: '1s', blurIni: '5px' })}
+    ${fadeIn({ time: '.5s', blurIni: '7px' })}
   `}
 `
 
