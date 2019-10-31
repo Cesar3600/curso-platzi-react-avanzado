a8eba47095d24e8169a7aa5e2e7f328760efb975
cesar <cesar.contreras.medina.3600@gmail.com>
2019-10-31 00:55:52 -0500

    [update] avance de la 16 hasta useref y observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,24 @@
 
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 import {ImgWrapper, Img, Button} from './styles'
 import { MdFavoriteBorder } from "react-icons/md";
 
 const IMG_DEFAULT = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes='?', src = IMG_DEFAULT }) => {
+    const referencia = useRef(null)
+
+    useEffect(() => {
+        const observer = new window.IntersectionObserver(entry => {
+            const {isIntersecting} = entry[0]
+            console.log(isIntersecting)
+        })
+        observer.observe(referencia.current)
+        //console.log(referencia.current)
+    }, [referencia])
+
     return(
-        <article>
+        <article ref={referencia}>
             <a href={`/detail/${id}`}>
                 <ImgWrapper>
                     <Img src={src}/>
