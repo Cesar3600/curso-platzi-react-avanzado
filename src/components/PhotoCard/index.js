d95f1433661921b1ef2421d85e894355d5ca5d5f
cesar <cesar.contreras.medina.3600@gmail.com>
2019-11-09 20:30:38 -0500

    clase 17 terminada

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,39 +1,48 @@
 
-import React,{useRef,useEffect,useState} from 'react'
+import React, {useRef,useEffect,useState} from 'react'
 import {ImgWrapper, Img, Button, Article} from './styles'
 import { MdFavoriteBorder } from "react-icons/md";
 
 const IMG_DEFAULT = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes='?', src = IMG_DEFAULT }) => {
-    const referencia = useRef(null)
+
+    const Element = useRef(null)
     const [show,setShow] = useState(false)
-    useEffect(() => {
-        Promise.resolve(
-            typeof window.IntersectionObserver != 'undefined' 
-            ? window.IntersectionObserver
-            : import('intersection-observer') 
-        ).then(()=>{
-            const observer = new window.IntersectionObserver(entry => {
-                const {isIntersecting} = entry[0]
-                isIntersecting && setShow(true)
+
+    useEffect(()=>{
+        Promise
+            .resolve(
+                window.IntersectionObserver == 'function'
+                ? window.IntersectionObserver
+                : import('intersection-observer')
+            )
+            .then(()=>{
+                //console.log(Element.current)
+                const Observer = new window.IntersectionObserver(response => {
+                    const { isIntersecting } = response[0]
+                    if (isIntersecting) {
+                        setShow(true)
+                    }
+                })
+                Observer.observe(Element.current)
             })
-            observer.observe(referencia.current)
-        })
-    }, [referencia])
+    },[Element])
 
     return(
-        <Article ref={referencia}>
+        <Article ref={Element}>
             {
-            show && <>
-            <a href={`/detail/${id}`}>
-                <ImgWrapper>
-                    <Img src={src}/>
-                </ImgWrapper>
-            </a>
-            <Button>
-                <MdFavoriteBorder size="32px"/>{likes} likes!
-            </Button></>
+                show &&
+                <>
+                    <a href={`/detail/${id}`}>
+                        <ImgWrapper>
+                            <Img src={src}/>
+                        </ImgWrapper>
+                    </a>
+                    <Button>
+                        <MdFavoriteBorder size="32px"/>{likes} likes!
+                    </Button>                
+                </>
             }
         </Article>
     )
